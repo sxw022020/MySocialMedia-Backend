44d36b20debfe604154de2d85860aaa659ebdf3c
haileysun22 <haileysun22@gmail.com>
2023-04-07 03:54:54 +0000

    add Post struct comments

--- model/model.go
+++ model/model.go
@@ -1,5 +1,11 @@
 package model
 
+/*
+*
+`struct`:
+  - a composite data type ("blueprint") that groups together zero or more named values of different types under a single name
+  - this is similar to an `object` in Java
+*/
 type Post struct {
 	Id      string `json:"id"`
 	User    string `json:"user"`
