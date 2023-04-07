a29075997e178cf2cebe334ddc4b8b90075e4c27
haileysun22 <haileysun22@gmail.com>
2023-04-07 05:05:55 +0000

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"MySocialMedia-Backend/handler"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	fmt.Println("Service started!")
+
+	/**
+	An HTTP server is started,
+	1. listening on port 8080 and
+	2. using the router initialized by the handler.InitRouter() function to handle incoming requests.
+	3. If the server encounters any errors, the log.Fatal function is used to log the error message and exit the program with a non-zero status code.
+	*/
+	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
 }
