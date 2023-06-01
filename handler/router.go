a56f948703341d9d08baeeebbdf16b506e98b73e
haileysun <haileysun22@gmail.com>
2023-06-01 15:20:00 +0000

    update notes in router.go

--- handler/router.go
+++ handler/router.go
@@ -4,8 +4,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Unlike Spring, go cannot suport annotation-based HTTP routing,
+// so we need our own method to map request URL to HTTP handler
+
 /*
-*
 Return Type: `*mux.Router` - a pointer to a `mux.Router` instance.
 */
 func InitRouter() *mux.Router {
