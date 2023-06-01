068e615fbb417933cb9ed5efc3d11a0f02f8d3d0
haileysun <haileysun22@gmail.com>
2023-06-01 16:39:29 +0000

    add comments for elasticsearch.go

--- backend/elasticsearch.go
+++ backend/elasticsearch.go
@@ -6,6 +6,9 @@ var (
 	ESBackend *ElasticsearchBackend
 )
 
+// `ElasticsearchBackend struct` represents a backend service for Elasticsearch.
+// It contains a pointer to an `elastic`.
+// `Client“ object which is a client connection to an Elasticsearch server.
 type ElasticsearchBackend struct {
 	Client *elastic.Client
 }
