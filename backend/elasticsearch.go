49ea51d848b4dce54b33a9faa91d63df6358bcc8
haileysun <haileysun22@gmail.com>
2023-06-02 16:47:50 +0000

    update elasticsearch.go and add comments

--- backend/elasticsearch.go
+++ backend/elasticsearch.go
@@ -6,11 +6,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/elastic/go-elasticsearch/esapi"
 	es7 "github.com/elastic/go-elasticsearch/v7"
 )
 
+/**
+This file: a Go program for interacting with an Elasticsearch backend
+**/
+
+// Declares a global variable ESBackend of type *ElasticsearchBackend.
+// This is the Elasticsearch client that will be used to communicate with the Elasticsearch server.
 var (
 	ESBackend *ElasticsearchBackend
 )
@@ -32,11 +39,29 @@ func InitElasticsearchBackend() {
 	}
 	client, err := es7.NewClient(cfg)
 	if err != nil {
+		/*
+			When the panic function is called,
+			it immediately stops the execution of the current function and
+			begins unwinding the stack of the goroutine,
+			running any deferred functions along the way.
+			If that unwinding reaches the top of the goroutine's stack, the program dies.
+		*/
 		panic(err)
 	}
 
+	/*
+		Once the client is successfully created,
+		it's encapsulated in an ElasticsearchBackend instance and
+		assigned to the global ESBackend.
+	*/
 	ESBackend = &ElasticsearchBackend{Client: client}
 
+	// mapping: JSON string that defines the structure of the index
+	// "keyword": `keyword`` fields are only searchable by their exact value
+	// 	- if you want to search for a specific user by username or password,
+	//    you'll need to provide the **exact** username or password,
+	//	  cannot use partial info to do search
+	// `"index": false`: it cannot be searched, but can still sppear in the results
 	checkAndCreateIndex(constants.POST_INDEX, `{
 		"mappings": {
 			"properties:" {
@@ -63,7 +88,13 @@ func InitElasticsearchBackend() {
 	fmt.Println("Indexes are created!")
 }
 
+/*
+This function checks:
+if a particular Elasticsearch index exists and
+if it doesn't, creates it.
+*/
 func checkAndCreateIndex(indexName string, mapping string) {
+	// check if the index exists
 	req := esapi.IndicesExistsRequest{
 		Index: []string{indexName},
 	}
@@ -72,29 +103,47 @@ func checkAndCreateIndex(indexName string, mapping string) {
 	if err != nil {
 		panic(err)
 	}
+	// halting execution and reports the error
+	// then close the body
 	defer res.Body.Close()
 
-	if res.StatusCode == 404 {
+	// `res.StatusCode == 404` means that the index does not exist
+	// `http.StatusNotFound` is 404
+	if res.StatusCode == http.StatusNotFound {
 		// Index does not exist, so create it
+		// - creating a new `IndicesCreateRequest` struct from the esapi package
 		req := esapi.IndicesCreateRequest{
 			Index: indexName,
-			Body:  bytes.NewReader([]byte(mapping)),
+			// sets the body of the request
+			// - converting the mapping (which is a string) into a byte slice and
+			//   then creating a new Reader for those bytes.
+			//   This Reader can then be read by the Elasticsearch client to send the data
+			Body: bytes.NewReader([]byte(mapping)),
 		}
 
+		// executing the request to create the index
+		// - using the Do method of the IndicesCreateRequest struct,
+		//   which sends the HTTP request to the Elasticsearch instance and
+		//   returns the response.
 		res, err := req.Do(context.Background(), ESBackend.Client)
 		if err != nil {
 			panic(err)
 		}
+		defer res.Body.Close() // defer the Close() call here
 
+		// checks if the HTTP response indicates an error
+		// (i.e., if the HTTP status code is 4xx or 5xx).
 		if res.IsError() {
+			//  declares a variable `e`` of type `map[string]interface{}``.
+			// This map will be used to store the JSON error response from the Elasticsearch instance
 			var e map[string]interface{}
+			// reading the body of the error response from the Elasticsearch instance,
+			// decoding the JSON into the e map, and checking if there was an error doing so
 			if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
 				panic(err)
 			}
 
 			fmt.Printf("[%s] Error indexing document ID=%d", res.Status(), e)
 		}
-
-		res.Body.Close()
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require github.com/gorilla/mux v1.8.0
 
 require (
+	github.com/elastic/go-elasticsearch v0.0.0
 	github.com/elastic/go-elasticsearch/v7 v7.17.10 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
