d9a609a0328bb154b1ee5035a07e7262ce99315c
haileysun <haileysun22@gmail.com>
2023-06-11 17:03:29 +0000

    add search router

--- handler/router.go
+++ handler/router.go
@@ -8,7 +8,7 @@ import (
 // so we need our own method to map request URL to HTTP handler
 
 /*
-Return Type: `*mux.Router` - a pointer to a `mux.Router` instance.
+InitRouter Return Type: `*mux.Router` - a pointer to a `mux.Router` instance.
 */
 func InitRouter() *mux.Router {
 	// Create a new Gorilla Mux router instance
@@ -16,6 +16,9 @@ func InitRouter() *mux.Router {
 
 	// 1. **Register** the `postUploadHandler` function as an HTTP handler for the "/upload" endpoint with the "POST" method
 	router.HandleFunc("/upload", postUploadHandler).Methods("POST")
+
+	// 1. **Register** the `searchHandler` function as an HTTP handler for the "/search" endpoint with the "GET" method
+	router.HandleFunc("/search", searchHandler).Methods("GET")
 
 	return router
 }
