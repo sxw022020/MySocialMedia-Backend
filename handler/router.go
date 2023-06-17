60f373c9db4d658084e5b7a72ba68bcd16f31e3e
haileysun <haileysun22@gmail.com>
2023-06-17 17:06:12 +0000

    add CORS to router

--- handler/router.go
+++ handler/router.go
@@ -1,24 +1,42 @@
 package handler
 
 import (
+	"MySocialMedia-Backend/util"
+	"net/http"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/form3tech-oss/jwt-go"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
-// Unlike Spring, go cannot suport annotation-based HTTP routing,
-// so we need our own method to map request URL to HTTP handler
+var mySigningKey []byte
 
-/*
-InitRouter Return Type: `*mux.Router` - a pointer to a `mux.Router` instance.
-*/
-func InitRouter() *mux.Router {
-	// Create a new Gorilla Mux router instance
+func InitRouter(config *util.TokenInfo) http.Handler {
+	mySigningKey = []byte(config.Secret)
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+
 	router := mux.NewRouter()
 
-	// 1. **Register** the `postUploadHandler` function as an HTTP handler for the "/upload" endpoint with the "POST" method
-	router.HandleFunc("/upload", postUploadHandler).Methods("POST")
+	// Endpoints not requiring authentication
+	router.HandleFunc("/signup", signupHandler).Methods("POST")
+	router.HandleFunc("/signin", signinHandler).Methods("POST")
 
-	// 1. **Register** the `searchHandler` function as an HTTP handler for the "/search" endpoint with the "GET" method
-	router.HandleFunc("/search", searchHandler).Methods("GET")
+	// Apply the middleware to endpoints requiring authentication
+	authRouter := router.PathPrefix("").Subrouter()
+	authRouter.Use(jwtMiddleware.Handler)
 
-	return router
+	authRouter.HandleFunc("/upload", postUploadHandler).Methods("POST")
+	authRouter.HandleFunc("/search", searchHandler).Methods("GET")
+
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	headersOk := handlers.AllowedHeaders([]string{"Authorization", "Content-Type"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "DELETE"})
+
+	return handlers.CORS(originsOk, headersOk, methodsOk)(router)
 }
