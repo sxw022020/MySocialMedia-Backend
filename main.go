dab1765baef3ae723b0ee1de128f5b047d6899db
haileysun <haileysun22@gmail.com>
2023-06-17 17:15:36 +0000

    add comments to main.go

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	fmt.Println("Service started!")
 
+	// load application configuration from deploy.yml
 	config, err := util.LoadApplicationConfig("conf", "deploy.yml")
 	if err != nil {
 		panic(err)
@@ -30,5 +31,6 @@ func main() {
 	2. using the router initialized by the handler.InitRouter() function to handle incoming requests.
 	3. If the server encounters any errors, the log.Fatal function is used to log the error message and exit the program with a non-zero status code.
 	*/
+	// The `config.TokenConfig` argument passed to `InitRouter` suggests that your routing configuration may depend on some sort of token information (possibly for authentication).
 	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter(config.TokenConfig)))
 }
