f3ccccd69299c61ac18c71f4184834d918262e08
haileysun <haileysun22@gmail.com>
2023-06-18 04:44:40 +0000

    remove notes in model.go

--- model/model.go
+++ model/model.go
@@ -1,14 +1,5 @@
 package model
 
-/*
-*
-`struct`:
-  - a composite data type ("blueprint") that groups together zero or more named values of different types under a single name
-  - this is similar to an `object` in Java
-
-`xxx`: raw string literals:
-  - can include any character that can appear in a text string, including control characters and special characters.
-*/
 type Post struct {
 	Id      string `json:"id"`
 	User    string `json:"user"`
